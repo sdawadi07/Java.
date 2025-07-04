f55595e8d8e026f4f7f2e054123f9df6e2b1280a
sdawadi07 <Swastikadawadi5865@gmail.com>
2025-07-04 12:45:16 -0600

    Learned to return values

--- src/week3.java
+++ src/week3.java
@@ -9,10 +9,28 @@ public class week3 {
             System.out.println(number + " is odd.");
         }
     }
+
+    public static int findFactorial(int num){
+        int result = 1;
+        for(int i = 1; i<=num; i++){
+            result *=i;
+            System.out.println("The factorial of " + num + " is " + result);
+        }
+        return result;
+
+
+
+    }
     public static void main(String[]args){
         //calling methods
 checkEven(8);
 checkEven(7);
 
+
+//Returning the values
+        findFactorial(5);
+        findFactorial(3);
+        findFactorial(10);
     }
+
 }
