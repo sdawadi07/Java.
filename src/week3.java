7358018ac97e348a1104f69e27924d09ba7797b1
sdawadi07 <Swastikadawadi5865@gmail.com>
2025-07-04 12:33:33 -0600

    Leaned about calling methods.

--- src/week3.java
+++ src/week3.java
@@ -1,7 +1,18 @@
 public class week3 {
+
+    public static void checkEven(int number){
+
+        if(number % 2==0){
+            System.out.println(number + " is even.");
+        }
+        else{
+            System.out.println(number + " is odd.");
+        }
+    }
     public static void main(String[]args){
         //calling methods
-
+checkEven(8);
+checkEven(7);
 
     }
 }
