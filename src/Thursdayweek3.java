a91bf1dd235f23ed8a537bcd0a0b87ced103b974
sdawadi07 <Swastikadawadi5865@gmail.com>
2025-07-07 22:18:22 -0600

    Concatenation

---
+++ src/Thursdayweek3.java
@@ -0,0 +1,11 @@
+public class Thursdayweek3 {
+    public static void main(String[] args){
+//Concatenation
+        String a = "Hello,";
+        String b = "My name is Swastika.";
+        String all = a + b;
+
+        System.out.println(all);
+
+    }
+}
